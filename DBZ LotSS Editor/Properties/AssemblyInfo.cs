d2df2c886e82c915d6b8d59d32938d2250b32f35
Dino Suvalic <82914521+MakoInfused@users.noreply.github.com>
2026-06-27 20:19:45 -0400

    attempting to fix github action workflow

--- DBZ LotSS Editor/Properties/AssemblyInfo.cs
+++ DBZ LotSS Editor/Properties/AssemblyInfo.cs
@@ -28,8 +28,6 @@ using System.Runtime.InteropServices;
 // Revision
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// <Assembly: AssemblyVersion("1.0.*")> 
 
 [assembly: AssemblyVersion("0.78.0")]
 [assembly: AssemblyFileVersion("0.78.0")]
